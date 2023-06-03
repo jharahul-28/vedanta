8ff5a7e400dd082782f22c6db5da3735527ac7fc
Deepak <deepakkumarkasyap12@gmail.com>
2023-06-03 17:33:06 +0530

    adding qr

--- src/App.js
+++ src/App.js
@@ -3,16 +3,19 @@ import About from './Components/QR/About';
 import Details from './Components/QR/Details/Details';
 import Documents from './Components/QR/Documents';
 import Navbar from './Components/QR/Navbar';
-import Landing from './Components/index/Landing';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Landing from './pages/Landing';
+import Qr from './pages/Qr';
 
 function App() {
   return (
     <>
-    {/* <Landing/> */}
-    {/* <Navbar/>
-    <About/>
-    <Documents/>
-    <Details/> */}
+      <BrowserRouter>
+        <Routes>
+            <Route path="/" element={<Landing />} />
+            <Route path="/Qr" element={<Qr />} />
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
